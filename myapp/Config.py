e57d1916a6086e1a9d9b3f67598ba820bf4fa643
Atishya Jain <atishya.jain@rocketmail.com>
2018-06-18 21:20:26 +0530

    Sample Config file added

---
+++ myapp/Config.py
@@ -0,0 +1,15 @@
+OauthTokenURL = "https://oauth.iitd.ac.in/token.php"
+ResourceURL = "https://oauth.iitd.ac.in/resource.php"
+CLIENT_ID = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CLIENT_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+AUTHORIZATION_CODE = "authorization_code"
+
+authLinkPart1 = "https://oauth.iitd.ac.in/authorize.php?response_type=code&client_id="
+authLinkPart2 = "&state=xyz"
+
+certiPath = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # Enter path to certificate (.crt file) on the server
+
+pswd = "xxxxxxxx"
+
+# Below this is a commented value that needs to be exported before running the scripts
+# export DJANGO_SETTINGS_MODULE=yearbook.settings
