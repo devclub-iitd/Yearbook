d4db0dd6dc9548b972b5af309f6103f64717770d
sohamvg <sohavmg11@gmail.com>
2021-07-10 19:00:19 +0530

    Fix latex issues

--- app/generate_single_personal_yb.py
+++ app/generate_single_personal_yb.py
@@ -40,7 +40,7 @@ FIRST_PAGE_LINES = 10
 NEXT_PAGE_LINES = 25
 
 from utils.emoji import emojis
-reps = {"\\": "\\textbackslash", "\n": "\\\\ ", "\r": "", "_": "\_", "~": "\~", "$": "\$", "{": "\{", "}": "\}", "&": "\&", "%": "\%", "^": "\\textsuperscript{$\wedge$}", "#": "\#"}
+reps = {"\\": "\\textbackslash", "\n": "\\\\[0pt] ", "\r": "", "_": "\_", "~": "\~", "$": "\$", "{": "\{", "}": "\}", "&": "\&", "%": "\%", "^": "\\textsuperscript{$\wedge$}", "#": "\#"}
 
 rep = {**reps, **emojis}
 rep = dict((re.escape(k), v) for k, v in rep.items())
