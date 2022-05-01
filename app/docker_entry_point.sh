c93beea1293b7e044a7403c30c9b1c59d7d34f48
RajatGupta02 <rajatgupta142002@gmail.com>
2022-05-01 23:55:17 +0530

    Updated docker_entry_point.sh

--- app/docker_entry_point.sh
+++ app/docker_entry_point.sh
@@ -26,8 +26,8 @@ python3 manage.py migrate
 python3 setAdminTable.py
 
 ## Add Users
-# python3 Scrape/Scrape.py
-# python3 CsvToDatabase.py
+python3 Scrape/Scrape.py
+python3 CsvToDatabase.py
 
 ## Commenting this so that polls are not added multiple times
 python3 addPolls.py
